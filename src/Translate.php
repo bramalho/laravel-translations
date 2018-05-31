5ffa5e08cf1a74702997730ec311c1ef7f369f12
Bruno Ramalho <bfmramalho@gmail.com>
2018-05-31 13:11:19 +0100

    translate trait

---
+++ src/Translate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BRamalho\LaravelTranslations;
+
+use Illuminate\Support\Facades\App;
+
+trait Translate
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function translations()
+    {
+        return $this->morphMany(Translation::class, 'translatable');
+    }
+
+    /**
+     * @return Translation
+     */
+    public function getTranslationAttribute()
+    {
+        return $this->translations->firstWhere('language', App::getLocale());
+    }
+}
